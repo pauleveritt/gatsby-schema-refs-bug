15fbf15fd97e068dd97b862fe7de4d7618d36eca
Paul Everitt <pauleveritt@me.com>
2020-12-14 10:17:55 -0500

    Remove comments.

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,33 +44,3 @@ exports.onCreateNode = ({
     return resourceNode
   }
 }
-
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions
-//   return
-//   const result = await graphql(`
-//     query {
-//       allResource {
-//         edges {
-//           node {
-//             fields {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//   return
-//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//     createPage({
-//       path: node.fields.slug,
-//       component: path.resolve(`./src/templates/tip.jsx`),
-//       context: {
-//         // Data passed to context is available
-//         // in page queries as GraphQL variables.
-//         slug: node.fields.slug
-//       }
-//     })
-//   })
-// }
